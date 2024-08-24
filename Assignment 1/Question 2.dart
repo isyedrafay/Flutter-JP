0ecbeb92a59ca3ec446a47d235e788abe7e7957a
Syed Rafay <117012642+isyedrafay@users.noreply.github.com>
2024-08-24 15:15:36 +0500

    Question 2

---
+++ Assignment 1/Question 2.dart
@@ -0,0 +1,17 @@
+
+void main() {
+  // Variables
+  String name = "Syed Rafay";      // Name (String)
+  int age = 23;                    // Age (int)
+  String studentClass = "3rd Year BSCS"; // Class (String)
+  double percentage = 85.5;        // Percentage (double)
+  bool previousResult = true;      // Previous Result: pass/fail (bool)
+  //writing comments for revision
+
+  // Output
+  print("Name: $name");
+  print("Age: $age years old");
+  print("Class: $studentClass");
+  print("Percentage: $percentage%");
+  print("Previous Result: ${previousResult ? 'Pass' : 'Fail'}");
+}
