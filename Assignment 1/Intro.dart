6ea23d89474400361b22b427a2cc0b7fb54b51f8
Syed Rafay <117012642+isyedrafay@users.noreply.github.com>
2024-08-24 15:12:00 +0500

    1st Question

---
+++ Assignment 1/Intro.dart
@@ -0,0 +1,7 @@
+void main() {
+  String name = "Syed Rafay";
+  int age = 23;
+  String university = "Ilma University";
+
+  print("My name is $name, I'm $age years old, and I'm a student at $university.");
+}
