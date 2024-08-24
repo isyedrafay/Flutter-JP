1ce12c07f798432d3df4260ce47813896dee05b3
Syed Rafay <117012642+isyedrafay@users.noreply.github.com>
2024-08-24 15:17:11 +0500

    4th

---
+++ Assignment 1/Question 4.dart
@@ -0,0 +1,11 @@
+void main() {
+  // Length and breadth of the rectangle
+  int length = 5;
+  int breadth = 7;
+
+  // Calculate the area
+  int area = length * breadth;
+
+  // Output the result
+  print("The area of the rectangle is $area square units.");
+}
