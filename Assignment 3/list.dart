49c2121dd61e4c579e7b24f048c8fcde6f2f3514
Syed Rafay <117012642+isyedrafay@users.noreply.github.com>
2024-09-07 23:42:35 +0500

    list

---
+++ Assignment 3/list.dart
@@ -0,0 +1,16 @@
+void main() {
+  List<int> number = [1, 23, 3, 14, 5, 61, 7, 8, 28];
+  number[8] = 22;
+  print(number[8]);
+  print(number.reversed);
+  print(number.any);
+//   number.shuffle;
+//   number.sort();
+//   number.addAll([20, 39, 40]);
+//   number.remove([1, 4]);
+//   print(number);
+//   List names = ["rafay", "abdullah", "hassan"];
+//   names.sort();
+//   names.shuffle();
+//   print(names);
+}
